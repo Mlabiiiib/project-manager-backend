faa1b80cbfcbd8c7947d5fff7fc75a6407dd8a35
Mlabiiiib <lamrabat.labib@etu.uae.ac.ma>
2023-05-11 23:32:44 +0100

    use better initialization data

--- docker/init.sql
+++ docker/init.sql
@@ -8,12 +8,14 @@ CREATE TABLE IF NOT EXISTS project
     status varchar(250) NOT NULL
 );
 
-INSERT INTO project (name, description, client, deadline, status) VALUES ('Project 1', 'Description 1', 'Client 1', '2023-10-31', 'TODO');
-INSERT INTO project (name, description, client, deadline, status) VALUES ('Project 2', 'Description 2', 'Client 2', '2023-12-12', 'TODO');
-INSERT INTO project (name, description, client, deadline, status) VALUES ('Project 3', 'Description 3', 'Client 3', '2023-01-31', 'TODO');
-INSERT INTO project (name, description, client, deadline, status) VALUES ('Project 4', 'Description 4', 'Client 4', '2023-03-30', 'DOING');
-INSERT INTO project (name, description, client, deadline, status) VALUES ('Project 5', 'Description 5', 'Client 5', '2023-08-03', 'DOING');
-INSERT INTO project (name, description, client, deadline, status) VALUES ('Project 6', 'Description 6', 'Client 6', '2023-07-15', 'DOING');
-INSERT INTO project (name, description, client, deadline, status) VALUES ('Project 7', 'Description 7', 'Client 7', '2023-02-27', 'DOING');
-INSERT INTO project (name, description, client, deadline, status) VALUES ('Project 8', 'Description 8', 'Client 8', '2023-03-18', 'DONE');
-INSERT INTO project (name, description, client, deadline, status) VALUES ('Project 9', 'Description 9', 'Client 9', '2023-05-05', 'DONE');
+
+
+INSERT INTO project (name, description, client, deadline, status)
+VALUES ('Create an ecommerce website', 'Description for Create an ecommerce website', 'TGV', '2023-05-12', 'TODO'),
+       ('Develop a mobile app', 'Description for Develop a mobile app', 'ABC Company', '2023-05-13', 'DOING'),
+       ('Redesign company website', 'Description for Redesign company website', 'XYZ Corporation', '2023-05-14', 'DONE'),
+       ('Implement CRM system', 'Description for Implement CRM system', 'Company ABC', '2023-05-15', 'TODO'),
+       ('Upgrade server infrastructure', 'Description for Upgrade server infrastructure', 'XYZ Corporation', '2023-05-16', 'DOING'),
+       ('Design marketing campaign', 'Description for Design marketing campaign', 'Company XYZ', '2023-06-09', 'DONE'),
+       ('Create social media strategy', 'Description for Create social media strategy', 'ABC Company', '2023-06-10', 'TODO'),
+       ('Develop customer loyalty program', 'Description for Develop customer loyalty program', 'TGV', '2023-06-11', 'DOING');
